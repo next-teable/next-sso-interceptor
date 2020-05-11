92ef1440c94b16b8b6b8952a43e62bbdc05b3fb0
vanish1984 <vanish1984@gmail.com>
2020-05-11 20:28:33 +0800

    sso-inteceptor

---
+++ src/main/java/studio/clouthink/next/ssointerceptor/SsoInterceptorConfiguration.java
@@ -0,0 +1,11 @@
+package studio.clouthink.next.ssointerceptor;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class SsoInterceptorConfiguration {
+
+
+}
---
+++ src/test/java/studio/clouthink/next/ssointerceptor/SsoInterceptorApplicationTests.java
@@ -0,0 +1,13 @@
+package studio.clouthink.next.ssointerceptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoInterceptorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
